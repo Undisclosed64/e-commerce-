b61f66ca29e65961be24e35b0f03109f0d87fd0a
Undisclosed <undisclosed674@gmail.com>
2022-09-23 12:38:21 -0700

    add deployment setup

--- app.js
+++ app.js
@@ -8,71 +8,8 @@ const cors = require("cors");
 const multer = require("multer");
 const path = require("path");
 
-const PORT = 5000 || process.env.PORT;
+const PORT = process.env.PORT || 5000;
 
-const a = () => {
-  app.use(express.static("uploads"));
-  const storage = multer.diskStorage({
-    destination: function (req, file, cb) {
-      cb(null, path.join(__dirname, "./uploads"));
-    },
-    filename: function (req, file, cb) {
-      cb(
-        null,
-        file.fieldname + "-" + Date.now() + file.originalname.match(/\..*$/)[0]
-      );
-    },
-  });
-  const multi_upload = multer({
-    storage,
-    fileFilter: (req, file, cb) => {
-      if (
-        file.mimetype == "image/png" ||
-        file.mimetype == "image/jpeg" ||
-        file.mimetype == "image/jpg"
-      ) {
-        cb(null, true);
-      } else {
-        cb(null, false);
-        const err = new Error("Only .jpg .jpeg .png images are supported!");
-        err.name = "ExtensionError";
-        return cb(err);
-      }
-    },
-  }).array("uploadImages", 10);
-  app.post("/api/upload-multiple", (req, res) => {
-    multi_upload(req, res, function (err) {
-      console.log(req.files);
-      //multer error
-      if (err instanceof multer.MulterError) {
-        console.log(err);
-        res
-          .status(500)
-          .send({
-            error: { msg: `multer uploading error: ${err.message}` },
-          })
-          .end();
-        return;
-      } else if (err) {
-        //unknown error
-        if (err.name == "ExtensionError") {
-          res
-            .status(413)
-            .send({ error: { msg: `${err.message}` } })
-            .end();
-        } else {
-          res
-            .status(500)
-            .send({ error: { msg: `unknown uploading error: ${err.message}` } })
-            .end();
-        }
-        return;
-      }
-      res.status(200).send("file uploaded");
-    });
-  });
-};
-a();
 //connect db
 const mongodb = process.env.MONGODB_URI;
 mongoose.connect(mongodb, { useNewUrlParser: true, useUnifiedTopology: true });
@@ -100,6 +37,14 @@ app.use(express.urlencoded({ extended: false }));
 
 //use routes
 app.use("/api", productRoute);
+
+if (process.env.NODE_ENV === "production") {
+  app.use(express.static("client/build"));
+
+  app.get("*", (req, res) => {
+    res.sendFile(path.resolve(__dirname, "client", "build", "index.html"));
+  });
+}
 
 app.listen(PORT, () => {
   console.log(`Server running on PORT ${PORT}`);
